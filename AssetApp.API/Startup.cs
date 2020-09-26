7d2de8539ba1df476b8a908051a993f849240a12
ethem <ethemcoskun@gmail.com>
2020-09-26 13:16:17 -0400

    bug fix

--- AssetApp.API/Startup.cs
+++ AssetApp.API/Startup.cs
@@ -46,7 +46,7 @@ namespace AssetApp.API
                         ValidateAudience = false
                     };
                 }
-            )
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
